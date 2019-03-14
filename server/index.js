f478dee146ea36109bbcb003b7d7f8e763b2c64a
Kupke <lincoln.kupke@libertymutual.com>
2019-03-13 21:51:29 -0400

    set up postgres client for the express server

--- server/index.js
+++ server/index.js
@@ -10,3 +10,21 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json())
 
+// Postgres Client Setup
+
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on('error', () => console.log('Lost PG connection'));
+
+
+// Setup of table for postgres if it doesnt exist
+pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
